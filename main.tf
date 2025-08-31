60089f106edcc4778b5c6349e6ebd1f83a9435e9
Minato <nekoneko.minato+work@gmail.com>
2025-08-31 20:16:06 +0900

    S3アクセスログ設定を追加

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "portfolio_site" {
-  bucket = "minato-portfolio-site-20250816"
+  bucket        = "minato-portfolio-site-20250816"
   force_destroy = false
 
   tags = {
@@ -27,11 +27,52 @@ resource "aws_s3_bucket_policy" "public_read" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
+        Effect    = "Allow",
         Principal = "*",
-        Action = ["s3:GetObject"],
-        Resource = "${aws_s3_bucket.portfolio_site.arn}/*"
+        Action    = ["s3:GetObject"],
+        Resource  = "${aws_s3_bucket.portfolio_site.arn}/*"
       }
     ]
   })
 }
+
+# アクセスログ設定
+
+resource "aws_s3_bucket" "access_log" {
+  bucket        = "minato-portfolio-logs-20250831"
+  force_destroy = false
+
+  tags = {
+    Name        = "PortfolioAccessLogs"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_policy" "access_log_policy" {
+  bucket = aws_s3_bucket.access_log.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowS3ServerLogDelivery",
+        Effect = "Allow",
+        Principal = {
+          Service = "logging.s3.amazonaws.com"
+        },
+        Action = [
+          "s3:PutObject"
+        ],
+        Resource = "${aws_s3_bucket.access_log.arn}/*",
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          },
+        }
+      }
+    ]
+  })
+}
+
+# AWSアカウント情報を取得するdataリソース
+data "aws_caller_identity" "current" {}
